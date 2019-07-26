7f5ca88dc64a5955619f80a990625b1d24b73288
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-26 21:27:05 +0100

    Test Runner

---
+++ Selenium-Python-Unitest-Template/tests/RunTest.py
@@ -0,0 +1,33 @@
+import unittest
+import argparse
+import sys
+from driverutil import Browser
+from pageobjects import GoogleSearchPage
+from pageobjects import SearchResultsPage
+
+
+class RunTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = Browser().getbrowser(browsername)
+        self.driver.get("https://www.google.com/")
+        self.googlesearchpage = GoogleSearchPage(self.driver)
+        self.searchresultspage = SearchResultsPage(self.driver)
+
+    def testExample(self):
+        self.googlesearchpage.searchfor("Selenium")
+        self.searchresultspage.link_selenium_present()
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    browser = argparse.ArgumentParser()
+    browser.add_argument("-b", "--browser", required=False,
+                         help="name of the browser", default="chrome")
+    browser.add_argument('unittest_args', nargs='*')
+    args = browser.parse_args()
+    sys.argv[1:] = args.unittest_args
+    browsername = vars(args)["browser"]
+    unittest.main()
