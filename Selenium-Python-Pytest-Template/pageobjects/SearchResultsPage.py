8940a4f618bd8bbd07a1b5486229c0485f639483
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-07-30 21:28:47 +0100

    Search Results Page

---
+++ Selenium-Python-Pytest-Template/pageobjects/SearchResultsPage.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+class SearchResultsPage:
+    LINKSELENIUM = (By.PARTIAL_LINK_TEXT, 'Selenium - Web Browser Automation')
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.linkselenium = driver.find_element(By.LINK_TEXT, "")
+
+    def link_selenium_present(self):
+        assert self.driver.find_element(*self.LINKSELENIUM).is_displayed()
