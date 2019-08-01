a68bbb314339c155546a2f906514928db54b6639
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-08-01 21:19:05 +0100

    Clean up

--- Selenium-Python-Pytest-Template/tests/RunTest.py
+++ Selenium-Python-Pytest-Template/tests/RunTest.py
@@ -1,17 +1,17 @@
 import pytest
+
 from pageobjects.GoogleSearchPage import GoogleSearchPage
 from pageobjects.SearchResultsPage import SearchResultsPage
 from tests.BaseTest import BaseTest
 
 
 @pytest.mark.incremental
-class RunTest(BaseTest):
+class test_run(BaseTest):
 
-    def __init__(self):
+    def test_example(self):
         self.searchresultspage = SearchResultsPage(self.driver)
         self.googlesearchpage = GoogleSearchPage(self.driver)
-
-    def testExample(self):
         self.driver.get("https://www.google.com/")
         self.googlesearchpage.searchfor("Selenium")
         self.searchresultspage.link_selenium_present()
+        pass
