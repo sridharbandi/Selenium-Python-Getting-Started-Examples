45be3fcfce9b1fa2554f19bbb588c4a5ca01269a
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-08-11 21:39:17 +0100

    Environments Updated

--- Selenium-Python-BehaveBDD-Template/features/environment.py
+++ Selenium-Python-BehaveBDD-Template/features/environment.py
@@ -1,15 +1,9 @@
 from driverutil.Browser import Browser
-from pageobjects.GoogleSearchPage import GoogleSearchPage
-from pageobjects.SearchResultsPage import SearchResultsPage
 
 
 def before_scenario(context, scenario):
-    if 'web' in context.tags:
-        context.browser = Browser().getbrowser('chrome')
-        context.googlesearchpage = GoogleSearchPage(context.browser)
-        context.searchresultspage = SearchResultsPage(context.browser)
+    context.browser = Browser().getbrowser('chrome')
 
 
 def after_scenario(context, scenario):
-    if 'web' in context.tags:
-        context.browser.quit()
+    context.browser.quit()
