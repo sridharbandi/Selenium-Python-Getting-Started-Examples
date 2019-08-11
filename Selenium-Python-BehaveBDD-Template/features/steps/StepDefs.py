9316e548d4a7450dcbc258dc45e6dc7b0b7aeb87
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-08-11 21:39:02 +0100

    Step Defs Updated

--- Selenium-Python-BehaveBDD-Template/features/steps/StepDefs.py
+++ Selenium-Python-BehaveBDD-Template/features/steps/StepDefs.py
@@ -1,14 +1,18 @@
 from behave import given, when, then
+from pageobjects.GoogleSearchPage import GoogleSearchPage
+from pageobjects.SearchResultsPage import SearchResultsPage
 
 
 @given(u'User is on Google search page')
 def step_impl_goto_google(context):
     context.browser.get("https://www.google.com/")
+    context.googlesearchpage = GoogleSearchPage(context.browser)
 
 
 @when(u'User searches for Selenium')
 def step_impl_search_selenium(context):
     context.googlesearchpage.searchfor("Selenium")
+    context.searchresultspage = SearchResultsPage(context.browser)
 
 
 @then(u'User can see Selenium results')
